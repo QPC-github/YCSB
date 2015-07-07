a293204c174be90ef6897a936ad0c62e0380a0d1
Joshua Thompson <thompsonja@google.com>
2015-07-06 18:02:51 -0700

    Fix columns for phase and workload, use weighted average for avg latency

--- vitess/scripts/merge_results.sh
+++ vitess/scripts/merge_results.sh
@@ -20,6 +20,6 @@ for i in $log_dir/*.csv; do
   # Average column 15 (Avg latency)
   # Min column 16 (Min latency)
   # Max columns 17, 18, 19 (max, p95, p99 latencies)
-  awk -F, -v var=$num_runners '{a[$1]+=$11;b[$1]+=$12;c[$1]+=$14;d[$1]+=$15;cnt++;c16[$1][cnt]=$16;c17[$1][cnt]=$17;c18[$1][cnt]=$18;c19[$1][cnt]=$19} END {for (i=0;i<FNR;i++) {asort(c16[i]);c17i=asort(c17[i]);c18i=asort(c18[i]);c19i=asort(c19[i]); print i,$2,$3,$4,$5,$6,$7,$8,$9,$10,a[i],b[i],$13,c[i],d[i]/var,c16[i][1],c17[i][c17i],c18[i][c18i],c19[i][c19i],var}}' OFS=',' ${basename}runner-*.csv
+  awk -F, -v var=$num_runners '{cnt++;c9[$1]=$9;c10[$1]+=$10;c11[$1]+=$11;c12[$1]+=$12;c13[$1]=$13;c14[$1]+=$14;c15[$1]+=$14*$15;c16[$1][cnt]=$16;c17[$1][cnt]=$17;c18[$1][cnt]=$18;c19[$1][cnt]=$19} END {for (i=0;i<FNR;i++) {asort(c16[i]);c17i=asort(c17[i]);c18i=asort(c18[i]);c19i=asort(c19[i]); print i,$2,$3,$4,$5,$6,$7,$8,c9[i],c10[i]/var,c11[i],c12[i],c13[i],c14[i],c15[i]/c14[i],c16[i][1],c17[i][c17i],c18[i][c18i],c19[i][c19i],var}}' OFS=',' ${basename}runner-*.csv
 done
 
